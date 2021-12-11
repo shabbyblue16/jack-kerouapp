08c4dfa266b8d6796a0dfd97ff38d62682a3e056
J <jbelding16@gmail.com>
2021-12-11 02:08:09 -0500

    Added driver table to db

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 
 import sequelize from '../db/index.js';
 
-const User = sequelize.define('users', {
+const Rider = sequelize.define('riders', {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
@@ -13,13 +13,20 @@ const User = sequelize.define('users', {
     type: Sequelize.STRING,
     allowNull: false,
   },
-  name: {
-    type: Sequelize.STRING,
-  },
   password: {
     type: Sequelize.STRING,
     allowNull: false,
   },
+  name: {
+    type: Sequelize.STRING,
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  location: {
+    type: Sequelize.POINT,
+  }
   // createdAt: {
   //   field: 'created_at',
   //   type: Sequelize.DATE,
@@ -32,4 +39,52 @@ const User = sequelize.define('users', {
   // },
 });
 
-export default User;
+const Driver = sequelize.define('drivers', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+  },
+  phone: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  location: {
+    type: Sequelize.POINT,
+  },
+  car: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  license: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  rating: {
+    type: Sequelize.DECIMAL,
+  }
+  // createdAt: {
+  //   field: 'created_at',
+  //   type: Sequelize.DATE,
+  //   defaultValue: Sequelize.NOW
+  // },
+  // updatedAt: {
+  //   field: 'updated_at',
+  //   type: Sequelize.DATE,
+  //   defaultValue: Sequelize.NOW,
+  // },
+});
+
+export default { Rider, Driver };
